87c29fd2d69fc9e2309dc4de6d6a4722abb77ac7
AgatheEdifixio <agathe.marcellini@edifixio.com>
2021-07-16 10:56:50 +0200

    logic

---
+++ src/README.md
@@ -0,0 +1,22 @@
+# Make route based on tickets Application 
+This function has been built to find the order of visited counties from an array of tickets. 
+
+The function accepts an array of tickets, where ticket has format [source: string,
+destination: string] and returns comma separated countries in order of visiting.
+
+# Features 
+To enter a new array of tickets, the user need replace the current array ("const tickets"  app.component.ts line16)
+
+Click the button "Find the route" on the webpage. 
+The result is display in the console
+
+# Launch the project 
+To execute the application on your computer, you need to install : 
+    NodeJS
+    Angular CLI 
+
+On the first launch use "npm install" 
+the use the command "ng serve" to run the project 
+
+# Technical stack 
+Angular 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routeBasedOnTickets';
+
+
+  // replace your array of tickets in the const "tickets"
+  callroute() {
+  const tickets = [["BER", "PAR"], ["JPN", "PHL"], ["BRA", "UAE"], ["USA", "BRA"], ["PHL", "BST"], ["UAE", "JPN"], ["PHL", "PAR"], ["BST", "BER"], ["BEL", "USA"]]; 
+  this.getRoute(tickets)
+  }
+
+
+  getRoute(ticketsList: any[]) {
+    const elements = ticketsList;
+    console.log("The inital array of tickets is : ", elements);
+    const copieElement = elements;
+    const finalTab: any[] = [];
+
+
+    for (const e of elements) {
+      const elemnt = copieElement.find(el => el[1] === e[0]);
+
+      if (elemnt === undefined) {
+        if (!finalTab.includes(e)) {
+          finalTab.push(e);
+        }
+      }
+    }
+
+    for (const f of finalTab) {
+      const lastItem = finalTab[finalTab.length - 1];
+
+      const elem = elements.find(el => el[0] === lastItem[1]);
+      if (elem != undefined) {
+        if (elem[0] === f[1]) {
+          if (!finalTab.includes(elem)) {
+            finalTab.push(elem)
+          }
+        }
+      }
+    }
+  
+    const newTab: any[] = [];
+
+    let result = finalTab.map(a => [
+      a[0],
+      newTab.push(a[0]),
+      a[1],
+      newTab.push(a[1]),
+
+    ]);
+    let unique = [...new Set(newTab)];
+    let finalResult = unique.join()
+   
+    console.log("The original route of the trip is : ", finalResult);
+  }
 }
+
+
